96368278aa57876210c51c1bfbca1b3b219bc4a4
RobMtank <siddhantydhyani99@gmail.com>
2023-02-23 00:26:42 +0530

    no component

--- src/components/Repos.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const Repos = () => {
-  return (
-    <div>Right Branch</div>
-  )
-}
-
-export default Repos
--- src/components/Search.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const Search = () => {
-  return (
-    <div>Left Branch</div>
-  )
-}
-
-export default Search
